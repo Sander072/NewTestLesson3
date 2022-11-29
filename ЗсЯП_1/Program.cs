64c33ed638f6037cecbb5bc1e0566085fe919bac
Sander072 <Sander072@mail.ru>
2022-11-29 21:13:35 +0300

    Задача 1

---
+++ ЗсЯП_1/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = int.Parse(Console.ReadLine());  
+int b = int.Parse(Console.ReadLine());  
+
+if (b * b == a)
+{
+    Console.WriteLine("Да");  
+}
+else
+{
+    Console.WriteLine("Нет");
+}
