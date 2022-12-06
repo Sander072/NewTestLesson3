1f24725a8ff6a4c3cc0b8f4a4533c0836fd4a266
Sander072 <Sander072@mail.ru>
2022-12-06 20:48:11 +0300

    Common

---
+++ ЗсЯП/Common/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Common;
+public static int max2 (int a, int b)
+{
+    if (a > b)
+    {
+        return a;
+    }
+    else
+    {
+        return b;
+    }
+}
+
+public static int GetNumberOfQuarter(int x, int y)
+    {
+        if (x > 0 && y > 0)
+        {
+            return 1;
+        }
+
+        if (x < 0 && y > 0)
+        {
+            return 2;
+        }
+
+        if (x < 0 && y < 0)
+        {
+            return 3;
+        }
+
+        if (x > 0 && y < 0)
+        {
+            return 4;
+        }
+
+        return -1;
+    }
