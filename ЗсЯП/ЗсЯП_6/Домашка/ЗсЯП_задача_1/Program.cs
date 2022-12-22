c25f678d2a6af4a625ab7f9e04f26ac7e0840580
Sander072 <Sander072@mail.ru>
2022-12-23 02:40:49 +0300

    домашняя работа 6

---
+++ ЗсЯП/ЗсЯП_6/Домашка/ЗсЯП_задача_1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Сколько чисел в массиве будет?");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите числа");
+
+int[] array = NewArray (a);
+GreaterThanZero (array, a);
+
+void GreaterThanZero (int[] x, int a)
+{
+    int n = 0;
+    for (int i = 0; i < a; i++)
+    {
+       if (x[i] > 0)
+       {
+            n = n + 1;
+       }
+    }
+    Console.WriteLine();
+    Console.WriteLine(n);
+}
+
+int[] NewArray (int x)
+{
+    int[] array = new int [x];
+    for (int i = 0; i < x; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
---
+++ ЗсЯП/ЗсЯП_6/Домашка/ЗсЯП_задача_2/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите b1");
+double b1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k1");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b2");
+double b2 = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите k2");
+double k2 = double.Parse(Console.ReadLine());
+
+IntersectionPoint (b1, k1, b2, k2);
+
+void IntersectionPoint (double b1, double k1, double b2, double k2)
+{
+    double x;
+    double y;
+
+    //k1*x+b1=k2*x+b2
+    //k1*x-k2*x=b2-b1
+    //x(k1-k2)=b2-b1
+    //x=(b2-b1)/(k1-k2)
+
+    if (k1-k2 != 0)
+    {
+        x = (b2-b1)/(k1-k2);
+        y = k1*x + b1;
+        Console.WriteLine();
+        Console.WriteLine("(" + x + ");(" + y + ")");
+    }
+    else
+    {
+        Console.WriteLine();
+        Console.WriteLine("Прямые с такими значениями не пересекаются"); 
+    }
+}
