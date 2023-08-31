72976d9f9fe9101b36540d6dc84b9a7866521314
Sander072 <Sander072@mail.ru>
2023-08-31 23:34:24 +0300

    домашка 2

---
+++ ИвПииО/Домашка_2/задание2.java
@@ -0,0 +1,17 @@
+package ИвПииО.Домашка_2;
+
+public class задание2 {
+    public static void fool(){
+        try {
+            int[] intArray = {1,2,3,4,5,6,7,8,9};
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+
+    }
+}
+
+
